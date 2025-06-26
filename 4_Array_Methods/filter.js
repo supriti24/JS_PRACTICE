e5387e08e2fd780f11c7acd1d0244594e9c94dae
supriti-github <misstripathi@gmail.com>
2025-06-26 06:32:58 +0530

    updated files filter and map

--- 4_Array_Methods/filter.js
+++ 4_Array_Methods/filter.js
@@ -1,7 +1,6 @@
 let numbers = [1, 2, 3, 4, 5];
+let oddNumbers = numbers.filter(num => num % 2 !== 0);  
 let evenNumbers = numbers.filter(num => num % 2 === 0);  
-console.log(evenNumbers);  // [2, 4]
+console.log("odd Numbers : "+ oddNumbers);  // [2, 4]
+console.log("even Numbers : "+evenNumbers); 
 
-let numbers = [1, 3, 5];
-let evenNumbers = numbers.filter(num => num % 2 === 0);  
-console.log(evenNumbers);  // [2, 4]
--- 4_Array_Methods/map.js
+++ 4_Array_Methods/map.js
@@ -1,3 +1,9 @@
 let numbers = [1, 2, 3];
 let doubled = numbers.map(num => num * 2);  
 console.log(doubled);  // [2, 4, 6]
+
+//  👉 Use map() to convert a list of test input values into formatted test data objects.
+let usernames = ['user1', 'user2'];
+let password = ['pwd1', 'pwd2'];
+let testUsers = usernames.map(u => ({ username: u, password: 'Test@123' }));
+console.log(testUsers); 
